5e447a45ab555400705be26e5981f788e10ed8a4
Muhammad Tariq <m_tariq.opt@hotmail.com>
2016-01-24 23:02:27 +0500

    Implementation of Trie Complete

--- src/main/java/datatypes/custom/Trie.java
+++ src/main/java/datatypes/custom/Trie.java
@@ -1,6 +1,8 @@
 package datatypes.custom;
 
 
+import java.security.InvalidKeyException;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -27,12 +29,12 @@ public class Trie {
      */
     private Integer value;
 
-    private final int SUBTREE_LENGTH = 63;
+    private final int SUBTREE_LENGTH = 94; // Number of printable ascii characters
 
     /**
      * Class constructor
      */
-    public Trie(){
+    public Trie() {
         this.count = 0;
         // @subtree could be a @HashMap, or an @ArrayList with initial size zero to avoid memory allocation. But when
         // we will call @get we either have to pass through a hashing algo or will have to iterate over @count number of
@@ -42,7 +44,7 @@ public class Trie {
     }
 
     public int size() {
-        return  count;
+        return count;
     }
 
     public boolean isEmpty() {
@@ -50,28 +52,95 @@ public class Trie {
     }
 
     public boolean containsKey(CharSequence key) {
-        return false;
+        if (key == null || key.length() == 0) {
+            return false;
+        }
+        if (subtree[this.getIndexFromASCII(key.charAt(0))] == null) {
+            return false;
+        }
+        if (key.length() == 1) {
+            return (subtree[this.getIndexFromASCII(key.charAt(0))].value != null);
+        } else {
+            return subtree[this.getIndexFromASCII(key.charAt(0))].containsKey(key.subSequence(1, key.length()));
+        }
     }
 
     public boolean containsValue(Integer value) {
+        if (value == null) {
+            throw new IllegalArgumentException("Value can not be null");
+        }
+        if (value.equals(this.value)) {
+            return true;
+        }
+        for (Trie entry : subtree) {
+            if (entry != null) {
+                if (entry.containsValue(value)) return true;
+            }
+        }
         return false;
     }
 
     public Integer get(CharSequence key) {
-        return null;
+        if (key == null || key.length() == 0) {
+            throw new IllegalArgumentException("Key length should be at least one character");
+        }
+        int index = this.getIndexFromASCII(key.charAt(0));
+        Character ch = key.charAt(0);
+        if (subtree[index] == null) {
+            return null;
+        }
+        if (key.length() == 1 && ch.equals(subtree[index].prefix)) { //Second condition is just a sanity check
+            return subtree[index].value;
+        }
+        return subtree[index].get(key.subSequence(1, key.length()));
     }
 
     public Integer put(CharSequence key, Integer value) {
-        return null;
+        if (key == null || key.length() == 0) {
+            throw new IllegalArgumentException("Key length should be at least one character");
+        }
+        Integer toReturn;
+        this.count++;
+        int index = this.getIndexFromASCII(key.charAt(0));
+        if (subtree[index] == null) {
+            subtree[index] = new Trie();
+        }
+        subtree[index].prefix = key.charAt(0);
+        if (key.length() == 1) {
+            toReturn = subtree[index].value;
+            subtree[index].value = value;
+        } else {
+            toReturn = subtree[index].put(key.subSequence(1, key.length()), value);
+        }
+
+        return toReturn;
     }
 
     public Integer remove(CharSequence key) {
-        return null;
+        if (key == null || key.length() == 0) {
+            throw new IllegalArgumentException("Key length should be at least one character");
+        }
+        Integer toReturn = null;
+        this.count--;
+        int index = this.getIndexFromASCII(key.charAt(0));
+        if (subtree[index] != null) {
+            if(key.length() == 1){
+                toReturn = subtree[index].value;
+                if(subtree[index].isEmpty()){
+                    subtree[index] = null;
+                } else {
+                    subtree[index].value = null;
+                }
+            } else {
+                toReturn = subtree[index].remove(key.subSequence(1, key.length()));
+            }
+        }
+        return toReturn;
     }
 
     public void putAll(Trie t) {
         Set<CharSequence> keySet = t.keySet();
-        for(CharSequence key: keySet){
+        for (CharSequence key : keySet) {
             this.put(key, t.get(key));
         }
     }
@@ -82,6 +151,27 @@ public class Trie {
     }
 
     public Set<CharSequence> keySet() {
-        return null;
+        Set<CharSequence> keySet = new HashSet<CharSequence>();
+        for (Trie entry : subtree) {
+            Set<CharSequence> entryKeySet;
+            if (entry != null) {
+                entryKeySet = entry.keySet();
+                if (this.prefix != null) {
+                    for (CharSequence key : entryKeySet) {
+                        keySet.add(this.prefix + key.toString());
+                    }
+                } else {
+                    keySet.addAll(entryKeySet);
+                }
+            }
+        }
+        if(this.value != null){
+            keySet.add(this.prefix.toString());
+        }
+        return keySet;
+    }
+
+    private int getIndexFromASCII(int ascii) {
+        return ascii - 33;
     }
 }
---
+++ src/test/java/datatypes/custom/TrieTest.java
@@ -0,0 +1,92 @@
+package datatypes.custom;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Set;
+
+/**
+ * Test class to test Trie
+ */
+public class TrieTest {
+
+    private Trie rootNode;
+
+    @Before
+    public void init(){
+        rootNode = new Trie();
+    }
+
+    @Test
+    public void testPutGet(){
+        rootNode.put("key", 1);
+        rootNode.put("keySpace", 3);
+        rootNode.put("keyValue", 4);
+        rootNode.put("secondKey", 2);
+        Assert.assertEquals(new Integer(1), rootNode.get("key"));
+        Assert.assertEquals(new Integer(3), rootNode.get("keySpace"));
+        Assert.assertEquals(new Integer(4), rootNode.get("keyValue"));
+        Assert.assertEquals(new Integer(2), rootNode.get("secondKey"));
+        Assert.assertNull(rootNode.get("Secondkey"));
+        Assert.assertEquals(4, rootNode.size());
+    }
+
+    @Test
+    public void testHasKeyValue(){
+        rootNode.put("uniqueKey", 30);
+        Assert.assertTrue(rootNode.containsKey("uniqueKey"));
+        Assert.assertFalse(rootNode.containsKey("noKey"));
+        Assert.assertTrue(rootNode.containsValue(30));
+        Assert.assertFalse(rootNode.containsValue(10));
+    }
+
+    @Test
+    public void testClear(){
+        rootNode.put("key", 1);
+        rootNode.put("keySpace", 3);
+        rootNode.put("keyValue", 4);
+        rootNode.put("secondKey", 2);
+        rootNode.clear();
+        Assert.assertTrue(rootNode.isEmpty());
+    }
+
+    @Test
+    public void testKeySet(){
+        rootNode.put("k", 1);
+        rootNode.put("ke", 3);
+        rootNode.put("ka", 4);
+        rootNode.put("kea", 4);
+        Set keySet = rootNode.keySet();
+        Assert.assertTrue(keySet.contains("ke"));
+        Assert.assertEquals(4, keySet.size());
+    }
+
+    @Test
+    public void testPutAll(){
+
+        rootNode.put("key", 1);
+        rootNode.put("keySpace", 3);
+        rootNode.put("keyValue", 4);
+        rootNode.put("secondKey", 2);
+
+
+        Trie test = new Trie();
+        test.putAll(rootNode);
+        Assert.assertTrue(test.containsKey("keySpace"));
+    }
+
+    @Test
+    public void testRemove(){
+        rootNode.put("key", 1);
+        rootNode.put("keySpace", 3);
+        rootNode.put("keyValue", 4);
+        rootNode.put("secondKey", 2);
+
+        Assert.assertEquals(1,(long)rootNode.remove("key"));
+        Assert.assertEquals(2,(long)rootNode.remove("secondKey"));
+        Assert.assertEquals(3,(long)rootNode.remove("keySpace"));
+        Assert.assertEquals(4,(long)rootNode.remove("keyValue"));
+        Assert.assertTrue(rootNode.isEmpty());
+    }
+}
